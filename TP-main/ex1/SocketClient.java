1816c03c5ad579dd1f022a0a2db197cec410de07
MedHabibManai <165595224+MedHabibManai@users.noreply.github.com>
2024-05-21 00:25:54 +0100

    Add files via upload

---
+++ TP-main/ex1/SocketClient.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+class SocketClient {
+    public static void main(String argv[]) {
+        int port = 0;
+        String host = "";
+       
